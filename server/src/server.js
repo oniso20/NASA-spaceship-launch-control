c63416439a564926b96e32933dd8c49e53e43ba2
Onis Emem <emem.c.onis@gmail.com>
2022-06-24 23:50:10 +0300

    Connected project to mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,23 @@ const { loadPlanetsData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-space-explorer-api:HkeQH3AbMy9rJvSZ@nasa-space-explorer-clu.tofth.mongodb.net/?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+//test mongoose database connection
+mongoose.connection.once('open', () => {
+    console.log('connected to mongoDB instance');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
+
+    //Load planets data
     await loadPlanetsData();
 
     server.listen(PORT, () => {
